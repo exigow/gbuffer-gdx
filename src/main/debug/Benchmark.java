77b4356b53553086d9983d87d17b9ff3337dc797
Jakub Błach <jakubblach@gmail.com>
2016-01-17 04:30:08 +0100

    Calculating total

--- src/main/debug/Benchmark.java
+++ src/main/debug/Benchmark.java
@@ -9,32 +9,44 @@ public class Benchmark {
   private final static double NANOSECONDS_TO_MILISECONDS = 1_000_000;
   private final static List<Task> TASKS = new ArrayList<>();
   private static Task ACTUAL_TASK;
+  private static long startTime;
 
   public static void start(String description) {
     if (ACTUAL_TASK != null)
       throw new IllegalStateException("Task " + ACTUAL_TASK.description + " not ended");
     ACTUAL_TASK = new Task();
     ACTUAL_TASK.description = description;
-    ACTUAL_TASK.startTime = now();
+    startTime = now();
   }
 
   public static void end() {
     if (ACTUAL_TASK == null)
       throw new IllegalStateException("No task in progress");
-    ACTUAL_TASK.endTime = now();
+    ACTUAL_TASK.time = now() - startTime;
     TASKS.add(ACTUAL_TASK);
     ACTUAL_TASK = null;
   }
 
   private static String formatTask(Task task) {
-    double executionTime = (double) (task.endTime - task.startTime) / NANOSECONDS_TO_MILISECONDS;
+    double executionTime = (double) task.time / NANOSECONDS_TO_MILISECONDS;
     return task.description + " " + String.format("%.2fms", executionTime);
   }
 
   public static String generateRaportAndReset() {
+    Task total = new Task();
+    total.description = "total";
+    total.time = calcTotalTime();
+    TASKS.add(total);
     String report = TASKS.stream().map(Benchmark::formatTask).collect(Collectors.joining(" -> "));
     TASKS.clear();
     return report;
+  }
+
+  private static long calcTotalTime() {
+    long result = 0;
+    for (Task t : TASKS)
+      result += t.time;
+    return result;
   }
 
   private static long now() {
@@ -44,8 +56,7 @@ public class Benchmark {
   private static class Task {
 
     public String description;
-    public long startTime;
-    public long endTime;
+    public long time;
 
   }
 
