8d7905c90d68de45753c1969667d650ea05f131f
Jakub Błach <jakubblach@gmail.com>
2016-01-23 21:13:39 +0100

    Blur effect tweaks

--- src/main/rendering/Blurer.java
+++ src/main/rendering/Blurer.java
@@ -2,6 +2,7 @@ package main.rendering;
 
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import main.rendering.filters.ShaderEffect;
 import main.rendering.utils.FrameBufferCreator;
 import main.rendering.utils.StaticFullscreenQuad;
 import main.resources.ResourceLoader;
@@ -9,7 +10,7 @@ import main.resources.ResourceLoader;
 public class Blurer {
 
   private static final int SIZE = 512;
-  private final ShaderProgram shader = ResourceLoader.loadShader("data/screenspace/screenspace.vert", "data/screenspace/blur-gauss.frag");
+  private final ShaderEffect shader = ShaderEffect.createGeneric("data/screenspace/blur-gauss.frag");
   private final FrameBuffer vertical = FrameBufferCreator.createDefault(SIZE, SIZE);
   private final FrameBuffer horizontal = FrameBufferCreator.createDefault(SIZE, SIZE);
 
@@ -23,16 +24,10 @@ public class Blurer {
   }
 
   private void blur(FrameBuffer from, FrameBuffer to, float x, float y) {
-    float texel = 1f / SIZE;
-    float[] offset = new float[] {texel * x, texel * y};
-    to.begin();
-    from.getColorBufferTexture().bind(0);
-    shader.begin();
-    shader.setUniformi("u_texture", 0);
-    shader.setUniform2fv("offset", offset, 0, offset.length);
-    StaticFullscreenQuad.renderUsing(shader);
-    shader.end();
-    to.end();
+    shader.renderTo(to)
+      .bind("u_texture", 0, from)
+      .paramterize("offset", x / SIZE, y / SIZE)
+      .flush();
   }
 
 }
