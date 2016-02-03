f272f731a7b0e81c164c2fed18e42d6386f77769
Jakub Błach <jakubblach@gmail.com>
2016-02-03 20:24:52 +0100

    lang fix

--- src/main/rendering/GBuffer.java
+++ src/main/rendering/GBuffer.java
@@ -17,8 +17,8 @@ public class GBuffer {
     ids = FrameBufferCreator.createDefault(512, 512);
   }
 
-  public static GBuffer withSize(int width, int heigth) {
-    return new GBuffer(width, heigth);
+  public static GBuffer withSize(int width, int height) {
+    return new GBuffer(width, height);
   }
 
   public void clearSubBuffers() {
