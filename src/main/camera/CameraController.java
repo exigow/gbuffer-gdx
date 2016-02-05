cbef2cc140aa8740993bf8a8e5e4fd96a5511ced
Jakub Błach <jakubblach@gmail.com>
2016-02-05 20:29:59 +0100

    3d camera

--- src/main/camera/CameraController.java
+++ src/main/camera/CameraController.java
@@ -2,29 +2,34 @@ package main.camera;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.math.collision.Ray;
 
 public class CameraController {
 
-  private final Vector3 eye = new Vector3(0, 0, 1);
+  private final Vector3 eye = new Vector3(0, 0, -512);
   private final Vector3 target = new Vector3(eye);
-  private final OrthographicCamera camera = new OrthographicCamera();
+  private final PerspectiveCamera camera;
   
   public static CameraController setUp(int width, int height) {
     return new CameraController(width, height);
   }
   
   private CameraController(int width, int height) {
-    camera.setToOrtho(true, width, height);
+    camera = new PerspectiveCamera(90, width, height);
+    camera.position.set(0, 0, -512);
+    camera.lookAt(0, 0, 0);
+    camera.near = 8;
+    camera.far = 1024;
+    camera.up.set(0, -1, 0);
+    camera.update();
   }
 
   public void update(float delta) {
     Vector3 movement = calcMovementVectorFromInput().scl(delta);
     target.add(movement);
-    target.z = clamp(target.z, .125f, 4);
+    target.z = clamp(target.z, -1024, -16);
     moveEyeToTarget(delta);
     updateOrthographicCameraState();
   }
@@ -32,7 +37,7 @@ public class CameraController {
   private static Vector3 calcMovementVectorFromInput() {
     Vector3 result = new Vector3();
     float planeScale = 512;
-    float zoomScale = 2;
+    float zoomScale = 512;
     if (isKey(Input.Keys.A))
       result.x -= planeScale;
     if (isKey(Input.Keys.D))
@@ -41,9 +46,9 @@ public class CameraController {
       result.y -= planeScale;
     if (isKey(Input.Keys.S))
       result.y += planeScale;
-    if (isKey(Input.Keys.E))
-      result.z -= zoomScale;
     if (isKey(Input.Keys.Q))
+      result.z -= zoomScale;
+    if (isKey(Input.Keys.E))
       result.z += zoomScale;
     return result;
   }
@@ -61,26 +66,25 @@ public class CameraController {
   }
 
   private void moveEyeToTarget(float delta) {
-    float scalar = .5f;
+    float scalar = .25f;
     eye.x += (target.x - eye.x) * scalar;
     eye.y += (target.y - eye.y) * scalar;
     eye.z += (target.z - eye.z) * scalar;
   }
 
   private void updateOrthographicCameraState() {
-    camera.position.set(eye.x, eye.y, 0);
-    camera.zoom = eye.z;
+    camera.position.z = eye.z;
+    camera.position.x = eye.x;
+    camera.position.y = eye.y;
     camera.update();
   }
 
-  public Vector2 unproject(float x, float y) {
-    Vector3 boxed = new Vector3(x, y, 0);
-    Vector3 unprojected = camera.unproject(boxed);
-    return new Vector2(unprojected.x, unprojected.y);
-  }
-
+  private final Vector3 tempVector = new Vector3();
   public Vector2 unprojectedMouse() {
-    return unproject(Gdx.input.getX(), Gdx.input.getY());
+    Ray pickRay = camera.getPickRay(Gdx.input.getX(), Gdx.input.getY());
+    Plane plane = new Plane(new Vector3(0, 0, 1), 0);
+    Intersector.intersectRayPlane(pickRay, plane, tempVector);
+    return new Vector2(tempVector.x, tempVector.y);
   }
 
   public Matrix4 matrix() {
